a3f4a7b6118cb72fbd04bf40a5c8dd3f4e8e07b4
marcelo <marcelo@contassconsultoria.com.br>
2022-07-16 18:14:39 -0300

    Gerenciando o estado

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -2,7 +2,7 @@
   <div class="box formulario">
     <div class="columns">
       <div
-        class="column is-8"
+        class="column is-5"
         role="form"
         aria-label="Formulario para criação de uma nova tarefa"
       >
@@ -13,6 +13,20 @@
           v-model="descricao"
         />
       </div>
+      <div class="column is-3">
+        <div class="select">
+          <select v-model="idProjeto">
+            <option value="">Selecione o projeto</option>
+            <option
+              :value="projeto.id"
+              v-for="projeto in projetos"
+              :key="projeto.id"
+            >
+              {{ projeto.nome }}
+            </option>
+          </select>
+        </div>
+      </div>
       <div class="column">
         <Temporizador @aoTemporizadorFinalizado="finalizarTarefa" />
       </div>
@@ -21,8 +35,11 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { computed, defineComponent } from "vue";
 import Temporizador from "./Temporizador.vue";
+import { useStore } from "vuex";
+import { key } from "@/store";
+
 export default defineComponent({
   name: "FormularioCriacao",
   emits: ["aoSalvarTarefa"],
@@ -32,16 +49,26 @@ export default defineComponent({
   data() {
     return {
       descricao: "",
+      idProjeto: "",
     };
   },
   methods: {
     finalizarTarefa(tempoEmSegundos: number): void {
-      this.$emit('aoSalvarTarefa', {
+      this.$emit("aoSalvarTarefa", {
         duracaoEmSegundos: tempoEmSegundos,
         descricao: this.descricao,
+        projeto: this.projetos.find(
+          (projeto) => projeto.id === this.idProjeto
+        ),
       });
       this.descricao = "";
     },
+  },
+  setup() {
+    const store = useStore(key);
+    return {
+      projetos: computed(() => store.state.projetos),
+    };
   },
 });
 </script>
@@ -51,5 +78,4 @@ export default defineComponent({
   color: var(--texto-primario);
   background: var(--bg-primario);
 }
-
 </style>
