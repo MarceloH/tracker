c53d91fc9480ab8b5f7c1ee065a9655fb622d61c
marcelo <marcelo@contassconsultoria.com.br>
2022-07-17 11:33:02 -0300

    Compilando a aplicação

--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -1,9 +1,14 @@
 <template>
   <Box>
     <div class="columns">
-      <div class="column is-7">{{ tarefa.descricao || 'Tarefa sem descrição' }}</div>
+      <div class="column is-4">
+        {{ tarefa.descricao || "Tarefa sem descrição" }}
+      </div>
+      <div class="column is-3">
+        {{ tarefa.projeto?.nome || "N/D" }}
+      </div>
       <div class="column">
-        <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos"/>
+        <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos" />
       </div>
     </div>
   </Box>
@@ -24,7 +29,7 @@ export default defineComponent({
   props: {
     tarefa: {
       type: Object as PropType<ITarefa>,
-      required: true
+      required: true,
     },
   },
 });
--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -23,6 +23,8 @@ import { defineComponent } from "vue";
 import { useStore } from "@/store";
 import { ALTERA_PROJETO, ADICIONA_PROJETO, NOTIFICAR } from "@/store/tipo-mutacoes"
 import { TipoNotificacao } from "@/interfaces/INotificacao";
+//import { notificacaoMixin } from "@/mixins/notificar";
+import useNotificador from "@/hooks/notificador"
 
 export default defineComponent({
   name: "FormularioView",
@@ -31,6 +33,7 @@ export default defineComponent({
       type: String,
     }
   },
+  //mixins: [notificacaoMixin],
   mounted () {
     if(this.id){
       const projeto = this.store.state.projetos.find(proj => proj.id == this.id)
@@ -54,18 +57,16 @@ export default defineComponent({
         this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto);  
       }
       this.nomeDoProjeto = "";
-      this.store.commit(NOTIFICAR, {
-        titulo: 'Novo projeto foi salvo',
-        texto: 'Prontinho ;) seu projeto já está diponível.',
-        tipo: TipoNotificacao.SUCESSO
-      })
+      this.notificar(TipoNotificacao.SUCESSO, 'Excelente!', 'O projeto foi cadastrado com sucesso');
       this.$router.push('/projetos')
-    },
+    }
   },
   setup() {
     const store = useStore();
+    const { notificar } = useNotificador();
     return {
-      store
+      store,
+      notificar
     };
   },
 });
