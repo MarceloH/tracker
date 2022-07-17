9d50caff93dbff9f0cfd9f268da577cde91e33cc
marcelo <marcelo@contassconsultoria.com.br>
2022-07-17 09:28:06 -0300

    Evoluindo o Alura Tracker

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,67 +1,20 @@
 <template>
-  <section class="projetos">
-    <h1 class="title">Projetos</h1>
-    <form @submit.prevent="salvar">
-      <div class="field">
-        <label for="nomeDoProjeto" class="label"> Nome Do Projeto </label>
-        <input
-          type="text"
-          class="input"
-          v-model="nomeDoProjeto"
-          id="nomeDoProjeto"
-          name="nomeDoProjeto"
-        />
-      </div>
-      <div class="field">
-        <button class="button" type="submit">Salvar</button>
-      </div>
-    </form>
-    <table class="table id-fullwidth">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Nome</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="projeto in projetos" :key="projeto.id">
-          <td>{{ projeto.id }}</td>
-          <td>{{ projeto.nome }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </section>
+  <div class="title">
+    <h1 class="projetos">Projetos</h1>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from "vue";
-import { useStore } from "@/store";
+import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "ProjetosView",
-  data() {
-    return {
-      nomeDoProjeto: ""
-    };
-  },
-  methods: {
-    salvar() {
-      this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto);
-      this.nomeDoProjeto = "";
-    },
-  },
-  setup() {
-    const store = useStore();
-    return {
-      store,
-      projetos: computed(() => store.state.projetos),
-    };
-  },
 });
 </script>
 
 <style scoped>
 .projetos {
-    padding: 1.25rem;
+  padding: 1.25rem;
 }
 </style>
